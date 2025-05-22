ab1eda7f0876a754ebd56067e94db570e8833a59
Ubani Solomon Ikedi <155390861+solo-ikedi@users.noreply.github.com>
2025-05-22 08:08:59 +0100

    fix: set base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === 'serve' ? '/' : '/Intern-hooks-task/', 
-  }
-});
+export default defineConfig({
+  plugins: [react()],
+  base: './', // ✅ THIS IS IMPORTANT
+})
